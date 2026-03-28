bfcfd9e939fc42f054cad731bb8c73ab95eb38e1
nandhakumarmurugesan <nandhakumarbems@gmail.com>
2026-03-28 14:46:57 +0530

    updated custom logger config to structlog

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,27 +1,63 @@
 import logging
 import os
 from datetime import datetime
+import structlog
+
 
 class CustomLogger:
     def __init__(self,log_dir="logs"):
         #check for log directory exists or not if not create one        
-        log_dir=os.path.join(os.getcwd(), "logs")
+        self.log_dir=os.path.join(os.getcwd(), "logs")
         os.makedirs(log_dir, exist_ok=True)
+        
         #create log file name with timestamp
         LOG_File=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-        LOG_File_Path=os.path.join(log_dir, LOG_File)
+        self.LOG_File_Path=os.path.join(log_dir, LOG_File)
+        
         #configure logging
-        logging.basicConfig(
-            filename=LOG_File_Path,
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            level=logging.INFO
-            )
+        # logging.basicConfig(
+        #     filename=LOG_File_Path,
+        #     format='%(asctime)s - %(levelname)s - %(message)s',
+        #     level=logging.INFO
+        #     )
 
     def get_logger(self,name=__file__):
-        return logging.getLogger(os.path.basename(name))
+        logger_name=os.path.basename(name)
+    
+        #configure logging for console and file in json format
+        
+        file_handler=logging.FileHandler(self.LOG_File_Path)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter("%(message)s"))
+        
+        console_handler=logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
+        
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(message)s',
+            handlers=[file_handler, console_handler]
+            
+        )
+
+        #configure structlog for JSON structure logging
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt="iso",utc=True,key="timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.JSONRenderer()
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use=True,
+        )
+        
+        return structlog.get_logger(logger_name)
 
 if __name__=="__main__":
     logger=CustomLogger()
     logger=logger.get_logger(__file__)
-    logger.info("Custom Logger Initialized. This is an info message.")
+    logger.info("user uploaded a pdf", user_id=123, file_name="document.pdf")
+    logger.error("Failed to process the document", user_id=123, error="File format not supported")
     
