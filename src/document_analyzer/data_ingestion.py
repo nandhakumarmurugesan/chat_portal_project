d6a97721b6ea4539151d68f3ac9bc502c82b23ea
nandhakumarmurugesan <nandhakumarbems@gmail.com>
2026-03-31 22:02:58 +0530

    data analysis updated

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -5,25 +5,51 @@ from datetime import datetime
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
-Class DocumentHandler:
+class DocumentHandler:
     """Handles pdf savign and reading operations
        Automatically logs all actions and supports session based organization
     """
-    def __init__(self):
-        """
-        Args:
-            base_dir (str, optional): _description_. Defaults to 'documents'.
-        """
-        pass
-    
+    def __init__(self, data_dir=None, session_id=None):
+        try:
+            self.log=CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv(
+                                    "DATA_STORAGE_PATH",
+                                    os.path.join(os.getcwd(), 'data',"document_analysis")
+                                )
+            self.session_id = session_id or f"session_{datetime.now().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            
+            #Create base session directory if it doesn't exist
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            os.makedirs(self.session_path, exist_ok=True)
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentHandler: {e}")
+            raise DocumentPortalException("Failed to initialize DocumentHandler:", e) from e
+        
+        self.log.info(
+                    f"PDFHandler initialized with session_id: {self.session_id}," 
+                    f"session_path: {self.session_path},"
+                    f"data_dir: {self.data_dir}" )
+            
     def save_pdf(self):
         """Saves the uploaded PDF file to the specified directory with a unique name.
            Logs the action and handles exceptions gracefully.
         """
-        pass
-    
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentHandler: {e}")
+            raise DocumentPortalException("Failed to initialize DocumentHandler:", e) from e
+        
     def read_pdf(self):
         """Reads the content of a PDF file and returns it as text.
            Logs the action and handles exceptions gracefully.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentHandler: {e}")
+            raise DocumentPortalException("Failed to initialize DocumentHandler:", e) from e
+        
+if __name__ == "__main__":
+    handler = DocumentHandler()
