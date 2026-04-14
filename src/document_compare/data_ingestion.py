e3074dabf145682dee8822e4ac0702d6c979235f
nandhakumarmurugesan <nandhakumarbems@gmail.com>
2026-04-14 15:18:54 +0530

    code compare updated

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -1,13 +1,13 @@
 import sys
-import pathlib import Path
+from pathlib import Path
 import fitz
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
 class DocumentIngestion:
-    def __init__(self, base_dir):
+    def __init__(self, base_dir:str="data\\document_compare"):
         self.log=CustomLogger().get_logger(__name__)
-        self.base_dir = Path(self.base_dir)
+        self.base_dir = Path(base_dir)
         self.base_dir.mkdir(parents=True, exist_ok=True)
         self.log.info(f"DocumentIngestion initialized successfully with base directory: {self.base_dir}")
                 
@@ -15,16 +15,38 @@ class DocumentIngestion:
         """Delete existing file in the specified path
         """
         try:
-            pass
+            if self.base_dir.exists() and self.base_dir.is_dir():
+                for file in self.base_dir.iterdir():
+                    if file.is_file():
+                        file.unlink() #delete the file 
+                        self.log.info(f"Deleted existing file: {file}")
+                self.log.info("All existing files deleted successfully and directory cleaned up", directory=str(self.base_dir))
         except Exception as e:
             self.log.error(f"Error deleting existing files: {e}")
             raise DocumentPortalException("An error occurred while deleting existing files:", sys)
 
-    def save_uploaded_files(self):
+    def save_uploaded_files(self, reference_file, actual_file):
         """Save uploaded files to the specified path
         """
         try:
-            pass
+            self.delete_existing_files()
+            self.log.info("Existing files deleted successfully")
+            
+            ref_path=self.base_dir / reference_file.name
+            act_path=self.base_dir / actual_file.name
+            
+            if not reference_file.name.endswith('.pdf'):
+                raise ValueError("Reference file must be a PDF")
+            
+            with open(ref_path, 'wb') as f:
+                f.write(reference_file.getbuffer())
+            
+            with open(act_path, 'wb') as f:
+                f.write(actual_file.getbuffer())
+            
+            self.log.info(f"Files saved successfully: reference={ref_path}, actual={act_path}")
+            return ref_path, act_path
+                        
         except Exception as e:
             self.log.error(f"Error saving uploaded files: {e}")
             raise DocumentPortalException("An error occurred while saving uploaded files:", sys)
@@ -48,3 +70,26 @@ class DocumentIngestion:
         except Exception as e:
             self.log.error(f"Error reading PDF file: {pdf_path}, error: {e}")
             raise DocumentPortalException(f"An error occurred while reading PDF file: {pdf_path}", sys)
+        
+    def combine_documents(self) -> str:
+        """Combine the content of reference and actual documents into a single string for comparison
+        """
+        try:
+            content_dict ={}
+            doc_parts =[]
+            
+            for filename in sorted(self.base_dir.iterdir()):
+                if filename.is_file() and filename.suffix == '.pdf':
+                    content_dict[filename.name] = self.read_pdf(filename)
+            
+            for filename, content in content_dict.items():
+                doc_parts.append(f"Document: {filename}\nContent:\n{content}")
+            
+            combined_text = "\n\n".join(doc_parts)
+            #f"Reference Document Content:\n{ref_content}\n\nActual Document Content:\n{act_content}"
+            self.log.info("Documents combined successfully", count=len(doc_parts))
+            return combined_text
+        
+        except Exception as e:
+            self.log.error(f"Error combining documents: {e}")
+            raise DocumentPortalException("An error occurred while combining documents:", sys)
--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -9,7 +9,6 @@ from prompt.prompt_library import PROMPT_REGISTRY
 from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_classic.output_parsers import OutputFixingParser
-from document_compare.data_ingestion import DocumentComparator
 
 class DocumentComparatorLLM:
     def __init__(self):
@@ -27,22 +26,32 @@ class DocumentComparatorLLM:
         self.chain = self.prompt | self.llm | self.parser 
         self.log.info("DocumentComparatorLLM initialized successfully")
     
-    def compare_documents(self):
+    def compare_documents(self, combined_docs:str) -> pd.DataFrame:
         """Compare the content of two documents and identify the differences
            The output should be in a structured format with page wise comparison and summary of changes
         """
         try:
-            pass
+            inputs = {
+                "combined_docs" : combined_docs,
+                "format_instructions" : self.parser.get_format_instructions()
+            }
+            self.log.info("Starting document comparison using LLM")
+            response = self.chain.invoke(inputs)
+            self.log.info("Document comparison completed successfully", response=response)
+            return self._format_response(response)
+        
         except Exception as e:
             self.log.error(f"Error in comparing documents: {e}")
             raise DocumentPortalException("An error occurred while comparing documents:", sys)    
     
-    def _format_response(self):
+    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame:
         """Format the response from the LLM into a structured format for easier consumption
         """
         try:
-            pass
+            df = pd.DataFrame(response_parsed)
+            self.log.info("Response formatted successfully into DataFrame", dataframe=df)
+            return df
         except Exception as e:
-            self.log.error(f"Error in formatting response: error=str(e)")
+            self.log.error(f"Error in formatting response: {e}")
             raise DocumentPortalException("An error occurred while formatting response:", sys)
     
